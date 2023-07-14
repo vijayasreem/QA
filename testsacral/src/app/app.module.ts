b8e4be0d68826682a1545d103c43300ab8840537
dotnet-code <@dotnet-code>
2023-07-14 10:57:52 +0000

    commit from code202307145753

--- testsacral/src/app/app.module.ts
+++ testsacral/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubConfigurationComponent } from './github-configuration/github-configuration.component';
 import { ConfigurationOfGithubComponent } from './configuration-of-github/configuration-of-github.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GithubConfigurationComponent,
-    ConfigurationOfGithubComponent
+    ConfigurationOfGithubComponent,
+    ConfigurationComponent
   ],
   imports: [
     BrowserModule,
---
+++ testsacral/src/app/configuration/configuration.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-configuration',
+  templateUrl: './configuration.component.html',
+  styleUrls: ['./configuration.component.css']
+})
+export class ConfigurationComponent implements OnInit {
+
+  errorMessage: string = 'NoneType' object has no attribute 'replace'';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
